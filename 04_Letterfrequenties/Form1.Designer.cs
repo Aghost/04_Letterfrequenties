37637f02c86dd5950913ab287a233d57d206013e
youri <a;sldkfjfjdksla>
2020-08-23 20:50:48 +0200

    resolved merge conflicts

--- 04_Letterfrequenties/Form1.Designer.cs
+++ 04_Letterfrequenties/Form1.Designer.cs
@@ -31,23 +31,16 @@
             this.btnLoadFile = new System.Windows.Forms.Button();
             this.textBox = new System.Windows.Forms.TextBox();
             this.btnDisplayFile = new System.Windows.Forms.Button();
-<<<<<<< HEAD
             this.btn_count = new System.Windows.Forms.Button();
-=======
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = new System.Windows.Forms.Button();
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.SuspendLayout();
             // 
             // btnLoadFile
             // 
             this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
             this.btnLoadFile.Name = "btnLoadFile";
-<<<<<<< HEAD
             this.btnLoadFile.Size = new System.Drawing.Size(97, 62);
-=======
-            this.btnLoadFile.Size = new System.Drawing.Size(123, 31);
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.btnLoadFile.TabIndex = 0;
             this.btnLoadFile.Text = "Load File";
             this.btnLoadFile.UseVisualStyleBackColor = true;
@@ -55,39 +48,24 @@
             // 
             // textBox
             // 
-<<<<<<< HEAD
             this.textBox.Location = new System.Drawing.Point(115, 12);
-=======
-            this.textBox.Location = new System.Drawing.Point(141, 12);
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
             this.textBox.ReadOnly = true;
             this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-<<<<<<< HEAD
             this.textBox.Size = new System.Drawing.Size(189, 313);
-=======
-            this.textBox.Size = new System.Drawing.Size(455, 510);
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.textBox.TabIndex = 2;
             // 
             // btnDisplayFile
             // 
-<<<<<<< HEAD
-            this.btnDisplayFile.Location = new System.Drawing.Point(12, 133);
+            this.btnDisplayFile.Location = new System.Drawing.Point(12, 138);
             this.btnDisplayFile.Name = "btnDisplayFile";
             this.btnDisplayFile.Size = new System.Drawing.Size(97, 62);
-=======
-            this.btnDisplayFile.Location = new System.Drawing.Point(12, 49);
-            this.btnDisplayFile.Name = "btnDisplayFile";
-            this.btnDisplayFile.Size = new System.Drawing.Size(123, 31);
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.btnDisplayFile.TabIndex = 4;
             this.btnDisplayFile.Text = "Display File";
             this.btnDisplayFile.UseVisualStyleBackColor = true;
             this.btnDisplayFile.Click += new System.EventHandler(this.btnDisplayFile_Click);
             // 
-<<<<<<< HEAD
             // btn_count
             // 
             this.btn_count.Location = new System.Drawing.Point(12, 263);
@@ -97,28 +75,20 @@
             this.btn_count.Text = "Count Characters";
             this.btn_count.UseVisualStyleBackColor = true;
             this.btn_count.Click += new System.EventHandler(this.btnDisplayChars_Click);
-=======
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 134);
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 31);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
             this.ClientSize = new System.Drawing.Size(318, 337);
             this.Controls.Add(this.btn_count);
-=======
-            this.ClientSize = new System.Drawing.Size(608, 534);
-            this.Controls.Add(this.button1);
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             this.Controls.Add(this.btnDisplayFile);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.btnLoadFile);
@@ -134,12 +104,10 @@
         private System.Windows.Forms.Button btnLoadFile;
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button btnDisplayFile;
-<<<<<<< HEAD
         private System.Windows.Forms.Button btn_count;
-=======
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button1;
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
+
     }
 }
 
--- 04_Letterfrequenties/Form1.cs
+++ 04_Letterfrequenties/Form1.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.IO;
-<<<<<<< HEAD
-using System.Runtime.CompilerServices;
-=======
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
 using System.Windows.Forms;
 
 namespace _04_Letterfrequenties
@@ -12,52 +8,39 @@ namespace _04_Letterfrequenties
     {
         int words = 0;
         int characters = 0;
-        int[] countChar = new int[255];
+
+        int[] countChar = new int[255] ;
+
+        string text = "";
 
         string NL = Environment.NewLine;
-        string text = "";
 
         public Form1()
         {
             InitializeComponent();
-<<<<<<< HEAD
  
             //string defaultText = String.Format($"words:\t\t\t{words}{NL}characters:\t\t{characters}{NL}");
             string defaultText = $"words:\t\t\t{words}{NL}characters:\t\t{characters}{NL}";
-=======
-            PrintResult();
-
-            string defaultText = String.Format($"words:\t\t\t{words}{NL}characters:\t\t{characters}\n\r{NL}");
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             textBox.Text = defaultText;
         }
 
         private void btnLoadFile_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             text = "";
             //string textfile = Directory.GetCurrentDirectory() + @"\file.txt";
 
             string textfile = @"C:\Users\v\source\repos\04_Letterfrequenties\04_Letterfrequenties\file.txt";
 
-=======
-            //string textfile = Directory.GetParent(Directory.GetCurrentDirectory()) + @"\..\..\..\file.txt";
-
-            string textfile = @"C:\Users\deploy\source\repos\04_Letterfrequenties\04_Letterfrequenties\file.txt";
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             text = File.ReadAllText(textfile);
+        }
 
-<<<<<<< HEAD
         private void btnDisplayFile_Click(object sender, EventArgs e)
         {
-=======
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
             textBox.Text = text;
         }
 
         private void btnDisplayChars_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             string addvalue = $"words:\t{words}{NL}characters:\t{characters}{NL}{NL}";
 
             if (text != "")
@@ -75,33 +58,6 @@ namespace _04_Letterfrequenties
             }
 
             textBox.Text += addvalue;
-=======
-            string output = "";
-            
-            if (text != "")
-            {
-                foreach (char character in text)
-                {
-                    countChar[character] += 1;
-                    output += $"| {character} : {countChar[character]} |";
-                }
-                output += $"{NL}";
-            }
-
-            int i = 32;
-            while (i < countChar.Length)
-            {
-                output += $"{i - 32}\t[{(char)i}] =\t" + countChar[i] + $"{NL}";
-                i++;
-            }
-
-            textBox.Text = output;
-        }
-
-        private void btnDisplayFile_Click(object sender, EventArgs e)
-        {
-            PrintResult();
->>>>>>> 0f03cb84746a6eb75d6d0b2336ef20e491824fff
         }
     }
 }
